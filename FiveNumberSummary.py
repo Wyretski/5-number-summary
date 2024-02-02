6141e033ae17adf9de51060722cc415d9294a975
Wyretski <138340925+Wyretski@users.noreply.github.com>
2024-02-02 10:38:57 -0800

    Update FiveNumberSummary.py

--- FiveNumberSummary.py
+++ FiveNumberSummary.py
@@ -1,27 +1,22 @@
 import statistics
 
 def calculate_five_number_summary(data):
-
-    print(data)
-
     order = sorted(data)
-    print(order)
-
     minimum = min(data)
-    print(f"Min: {minimum}")
-
-    quartile_2 = statistics.median(data)
-    print(f"Q2: {quartile_2}")
-
     maximum = max(data)
-    print(f"Max: {maximum}")
-
+    quartile_2 = statistics.median(data)
     mean = (sum(data) / len(data))
-    print(f"Mean: {mean}")
-
     standard_deviation = statistics.stdev(data)
+
+    print(f"Unordered: {data}")
+    print(f"Ordered: {order}")
+    print(f"Min: {min}")
+    print(f"Q1: {}")
+    print(f"Q2: {quartile_2}")
+    print(f"Q3: {}")
+    print(f"Mean: {mean}")
     print(f"Standard Deviation: {standard_deviation}")
 
 calc = calculate_five_number_summary
 calc([array])
-#Replace array with an actual array (e.g. [1, 2, 3, 4, 5])
+#Replace [array] with an actual array (e.g. [1, 2, 3, 4, 5])

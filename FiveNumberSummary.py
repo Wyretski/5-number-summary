a3469e3d65c457e40c37a0a7379f5eb4ea611902
Wyretski <138340925+Wyretski@users.noreply.github.com>
2024-02-03 21:46:10 -0800

    Update FiveNumberSummary.py

--- FiveNumberSummary.py
+++ FiveNumberSummary.py
@@ -8,24 +8,25 @@ def calculate_five_number_summary(data):
     quartile_2 = statistics.median(data)
     mean = (sum(data) / len(data))
     standard_deviation = statistics.stdev(data)
+
+    print(f"Length: {length}")
+    print(f"Unordered: {data}")
+    print(f"Ordered: {order}")
+    print(f"Min: {minimum}")
+    print(f"Q2: {quartile_2}")
+    print(f"Max: {maximum}")
+    print(f"Mean: {mean}")
+    print(f"Standard Deviation: {standard_deviation}")
     
     if length % 2 != 0:
         order.pop(quartile_2)
     
     quartile_1 = statistics.median(order[:length//2])
     quartile_3 = statistics.median(order[length//2:])
-
-    print(f"Length: {length}")
-    print(f"Unordered: {data}")
-    print(f"Ordered: {order}")
-    print(f"Min: {minimum}")
+   
     print(f"Q1: {quartile_1}")
-    print(f"Q2: {quartile_2}")
     print(f"Q3: {quartile_3}")
-    print(f"Max: {maximum}")
-    print(f"Mean: {mean}")
-    print(f"Standard Deviation: {standard_deviation}")
 
 calc = calculate_five_number_summary
-calc([0, 1, 2, 3, 4, 5, 6])
+calc([array])
 #Replace [array] with an actual array (e.g. [1, 2, 3, 4, 5])
